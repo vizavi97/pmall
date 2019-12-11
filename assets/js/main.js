6c506be1dc9cd8910c8fc2086fc2d5953c2c577d
PeaJIucm <PeaJIucm@gmail.com>
2019-12-11 10:50:02 +0300

    Last commit (11.12 morning)

--- assets/js/main.js
+++ assets/js/main.js
@@ -165,8 +165,7 @@ function isNumberKey(evt) {
   return true;
 };
 function isNumberPhoneKey(key)  {
-  return (key >= '0' && key <= '9') || key == '+' || key == '(' || key == ')' || key == '-' ||
-    key == 'ArrowLeft' || key == 'ArrowRight' || key == 'Delete' || key == 'Backspace';
+  return (key >= '0' && key <= '9') || key == '+' || key == 'ArrowLeft' || key == 'ArrowRight' || key == 'Delete' || key == 'Backspace';
 };
 
 $(document).on('click', '.add-product', function () {
